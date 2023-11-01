4b5dd3fe6daaa98d1f1f3ebf06f05a9e6041465d
megaManX6 <tiagu.pereira@hotmail.com>
2023-11-01 10:39:50 -0300

    foi

--- Bolinha.cpp
+++ Bolinha.cpp
@@ -2,5 +2,38 @@
 #include <string.h>
 #include <iomanip>
 #include <math.h>
-#include "stack.h"
+
+
+
+#define TAM 6
+
 using namespace std;
+
+typedef int stack_element;
+
+#include "stack.h"
+
+typedef struct {
+    int numero_elementos;
+    Stack pilha;
+} Tubo;
+
+int main(){
+    Tubo T [TAM];
+    int repetir = 1, retorno;
+    do{
+        iniciar_vazias(T);
+        distribuir(T);
+        mostrar(T);
+        do{
+            retorno = jogada(T);
+            mostrar(T);
+            if(retorno == 0)break;
+        }while(retorno);
+        mostrar(T);
+        cout<<"\n P A R A B E N S ! ! !";
+        cout<<"\n Jogar Novamente? 1(sim) ou 0(nao): ";cin>> repetir;
+    }while(repetir);
+    cout<<"Fim do JOGO"<<endl;
+    return;
+}
