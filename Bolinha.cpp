8b7a95dec177583074194d6cf8bc4325a49f8b47
VinMendes <vinicius.mc6@puccampinas.edu.br>
2023-11-01 10:51:15 -0300

    Funções Dadas

--- Bolinha.cpp
+++ Bolinha.cpp
@@ -2,21 +2,46 @@
 #include <string.h>
 #include <iomanip>
 #include <math.h>
-
-
+#include <windows.h>
 
 #define TAM 6
-
 using namespace std;
 
 typedef int stack_element;
-
 #include "stack.h"
 
 typedef struct {
     int numero_elementos;
     Stack pilha;
 } Tubo;
+
+void mudacor(int cor) {
+    /*
+    0 = Preto           8 = Cinza
+    1 = Azul            9 = Azul Claro
+    2 = Verde           A = Verde Claro
+    3 = Verde-Aguia     B = Verde-Aguia Claro
+    4 = Vermelho        C = Vermelho Claro
+    5 = Roxo            D = Lilas
+    6 = Amarelo         E = Amarelo Claro
+    7 = Branco          F = Branco Brilhante
+    */
+}
+
+void printa_cor(int I) {
+    int cor[5]={9,13,2,15,6};
+    mudacor(cor[I-1]);
+    cout<<'O';
+    mudacor(15);
+}
+
+void iniciar_vazias(Tubo T[]) {
+    for(int ax=0; ax<TAM; ax++) {
+        initStack(T[ax].pilha);
+        T[ax].numero_elementos=0;
+    }
+}
+
 
 int main(){
     Tubo T [TAM];
