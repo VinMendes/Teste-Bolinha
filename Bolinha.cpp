dece86060575e45f7aa44594b141073c9f289b67
VinMendes <vinicius.mc6@puccampinas.edu.br>
2023-11-05 21:26:16 -0300

    valida

--- Bolinha.cpp
+++ Bolinha.cpp
@@ -38,22 +38,53 @@ void distribuir(Tubo T[]){
 }
 
 void mostrar (Tubo T[]){
-    Tubo T_aux[TAM];
-    stack_element v;
-        
-    for (int i=0 ; i<(TAM-1) ; i++){
-
-        for (int j=0 ; j<(TAM-1) ; j++){ 
-            v = pop(T[j].pilha);
-            if (v){
-                cout<<"|"<<v<<"|  ";
-            }else{
-                cout<<"| | ";
+    Stack P_ax;
+    stack_element v, v2;
+    for (int ax = 0; ax < TAM-1;ax++) {
+        for (int ax2 = 0; ax2 < TAM; ax2++) {
+            if(isEmpty(T[ax2].pilha)) {
+                cout<<"|   | ";
+            }
+            else {
+                v2=pop(T[ax2].pilha);
+                cout<<"| "<<v2<<" | ";
             }
         }
-        cout<<"\n";
+        cout<<endl;
     }
 }
+
+int validar (Tubo T[], int o, int d) {
+    stack_element v, v2;
+    if (T[d].numero_elementos < TAM && T[o].numero_elementos > 0) {
+        v = pop(T[o].pilha);
+        T[o].numero_elementos--;
+        push(T[d].pilha, v);
+        T[d].numero_elementos++;
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+/*
+int validar_fim(Tubo T[]) {
+    Tubo axi;
+    int vazio;
+    stack_element V, V2;
+    for(int ax = 0; ax <= TAM; ax++) {
+        axi = T[ax];
+        vazio = isEmpty(axi.pilha);
+        if (vazio == 0 && axi.numero_elementos == 5) {
+            V = peek(axi.pilha);
+            for(int ax2 = 0; ax2 )
+            V2 = pop(axi.pilha);
+
+        }
+
+    }
+}
+*/
 void mudacor(int cor) {
     /*
     0 = Preto           8 = Cinza
@@ -65,6 +96,10 @@ void mudacor(int cor) {
     6 = Amarelo         E = Amarelo Claro
     7 = Branco          F = Branco Brilhante
     */
+
+    HANDLE saida;
+    saida = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(saida, cor);
 }
 
 void printa_cor(int I) {
@@ -89,7 +124,8 @@ int main(){
     iniciar_vazias(T);
 
     distribuir(T);
-
+    v = pop(T[2].pilha);
+    T[2].numero_elementos-1;
     mostrar(T); 
 
 
